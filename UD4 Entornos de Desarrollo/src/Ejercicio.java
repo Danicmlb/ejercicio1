05ffff64089525a055732d1e10e3db78560e53ad
Daniel <Daniel@Ryu-Laptop>
2022-03-11 12:19:22 +0100

    Trabajar con repositorio GitHub - Ejercicio

---
+++ UD4 Entornos de Desarrollo/src/Ejercicio.java
@@ -0,0 +1,13 @@
+public class Ejercicio {
+
+	public static String mensaje;
+	
+	public static void main(String[] args) {
+		
+		mensaje = "En este proyecto trabajamos con el repositorio GITHUB.";
+		
+		System.out.println(mensaje);
+
+	}
+
+}
